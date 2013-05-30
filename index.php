ee97b3449a8328f8a57cf4a50c04d76ab3dbcc26
Andrew Jackson <lordmuffin@outlook.com>
2013-05-29 20:07:14 -0500

    Merge https://github.com/WordPress/WordPress
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,38 +1,17 @@
 <?php
 /**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * Please see /external/starkers-utilities.php for info on get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
+ * @package WordPress
  */
-?>
-<?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<div class="col col-2-3">
-<?php if ( have_posts() ): ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-<?php while ( have_posts() ) : the_post(); ?>
-
-		<div class="module">
-			<h2 class="posttitle"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<h5>Published <?php the_time('F j, Y'); ?> by <?php echo get_the_author() ; ?></h5>
-			<hr></hr>
-			<?php the_content(); ?>
-			<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?> <?php edit_post_link('edit', '(', ')'); ?>
-		</div>
-
-<?php endwhile; ?>
-
-<?php else: ?>
-<h2>No posts to display</h2>
-<?php endif; ?>
-</div>
-
-<?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" class="searchform" action="<?php echo home_url( '/' ); ?>">
-		<input type="text" value="" name="s" class="s" />
-		<button type="submit" class="searchsubmit formbutton">Search</button>
-</form>
